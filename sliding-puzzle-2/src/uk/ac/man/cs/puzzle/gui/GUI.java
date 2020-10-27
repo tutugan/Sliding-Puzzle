1a2923aa1d8c94cd8dd4aff191166d198ec6fa34
Suzanne M. Embury <suzanne.m.embury@manchester.ac.uk>
2020-10-27 15:30:45 +0000

    Add timer information to game front end
    The GUI uses a Swing timer to notify the game every second.  These
    notifications are then used to maintain the game timer in the puzzle
    model.

--- sliding-puzzle-2/src/uk/ac/man/cs/puzzle/gui/GUI.java
+++ sliding-puzzle-2/src/uk/ac/man/cs/puzzle/gui/GUI.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import uk.ac.man.cs.puzzle.logic.Model;
 
@@ -16,6 +18,7 @@ public class GUI extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private final Model puzzleModel;
 	private GraphicsPanel puzzleGraphics;
+	private Timer gameTimer;
 	private int ROWS;
 	private int COLS;
 
@@ -23,6 +26,12 @@ public class GUI extends JPanel {
 		// Create a button. Add a listener to it.
 		JButton newGameButton = new JButton("New Game");
 		newGameButton.addActionListener(new NewGameAction());
+
+		// Create game timer components
+		JLabel timerLabel = new JLabel("Time: ", JLabel.LEADING);
+		final JLabel currentTimeLabel = new JLabel(" __ ", JLabel.CENTER);
+		currentTimeLabel.setText(String.valueOf("0"));
+		JLabel unitsLabel = new JLabel(" seconds", JLabel.LEADING);
 
 		// Create control panel
 		JPanel controlPanel = new JPanel();
@@ -35,10 +44,37 @@ public class GUI extends JPanel {
 		puzzleModel = new Model(ROWS, COLS);
 		puzzleGraphics = new GraphicsPanel(puzzleModel, rows, cols);
 
+		// Create game timer panel
+		JPanel gameTimerPanel = new JPanel();
+		gameTimerPanel.setLayout(new FlowLayout());
+		gameTimerPanel.add(timerLabel);
+		gameTimerPanel.add(currentTimeLabel);
+		gameTimerPanel.add(unitsLabel);
+
 		// Set the layout and add the components
 		this.setLayout(new BorderLayout());
 		this.add(controlPanel, BorderLayout.NORTH);
 		this.add(puzzleGraphics, BorderLayout.CENTER);
+		this.add(gameTimerPanel, BorderLayout.SOUTH);
+
+		// Set up the Swing timer
+		gameTimer = new Timer(1000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (puzzleModel.gameOver()) {
+					if (gameTimer.isRunning())
+						gameTimer.stop();
+				} else {
+					puzzleModel.incrementGameTime();
+					currentTimeLabel.setText(String.valueOf(puzzleModel.getGameTime()));
+					unitsLabel.setText(puzzleModel.getGameTime() == 1 ? " second" : " seconds");
+				}
+			}
+		});
+
+		// Start the timer for the first game round
+		gameTimer.start();
+
 	}
 
 	Model getPuzzleModel() {
@@ -49,12 +85,18 @@ public class GUI extends JPanel {
 		return puzzleGraphics;
 	}
 
+	Timer getGameTimer() {
+		return gameTimer;
+	}
+
 	public class NewGameAction implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			puzzleModel.reset();
 			puzzleModel.shuffle();
 			puzzleGraphics.repaint();
 			puzzleGraphics.setBackground(Color.black);
+
+			gameTimer.restart();
 		}
 	}
 }
