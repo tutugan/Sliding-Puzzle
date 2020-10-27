c1d2d8943a2cae3943646acce66d346cf968ebbf
Suzanne M. Embury <suzanne.m.embury@manchester.ac.uk>
2020-10-27 11:56:39 +0000

    Initial import

---
+++ sliding-puzzle-2/src/uk/ac/man/cs/puzzle/gui/GUI.java
@@ -0,0 +1,60 @@
+package uk.ac.man.cs.puzzle.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import uk.ac.man.cs.puzzle.logic.Model;
+
+public class GUI extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private final Model puzzleModel;
+	private GraphicsPanel puzzleGraphics;
+	private int ROWS;
+	private int COLS;
+
+	public GUI(int rows, int cols) {
+		// Create a button. Add a listener to it.
+		JButton newGameButton = new JButton("New Game");
+		newGameButton.addActionListener(new NewGameAction());
+
+		// Create control panel
+		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout(new FlowLayout());
+		controlPanel.add(newGameButton);
+
+		// Create graphics panel
+		ROWS = rows;
+		COLS = cols;
+		puzzleModel = new Model(ROWS, COLS);
+		puzzleGraphics = new GraphicsPanel(puzzleModel, rows, cols);
+
+		// Set the layout and add the components
+		this.setLayout(new BorderLayout());
+		this.add(controlPanel, BorderLayout.NORTH);
+		this.add(puzzleGraphics, BorderLayout.CENTER);
+	}
+
+	Model getPuzzleModel() {
+		return puzzleModel;
+	}
+
+	GraphicsPanel getGraphicsPanel() {
+		return puzzleGraphics;
+	}
+
+	public class NewGameAction implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			puzzleModel.reset();
+			puzzleModel.shuffle();
+			puzzleGraphics.repaint();
+			puzzleGraphics.setBackground(Color.black);
+		}
+	}
+}
